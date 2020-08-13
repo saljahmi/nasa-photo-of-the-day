4b0a90aba51f318d7603ef40f2edc6c8169951df
saljahmi <saljahmi@gmail.com>
2020-08-13 18:52:22 -0400

    resolved asynchronous issue, components rendered successfully

--- src/App.js
+++ src/App.js
@@ -8,14 +8,11 @@ export default function App (props) {
   const [image, setImage] = useState(null)
 
   useEffect(() => {
-    // use axios
-    // on success breakpoint
-    // on failure breakpoint
 
     axios.get('https://api.nasa.gov/planetary/apod?api_key=KT5wuG2rgD0xURYm7T7IexjWq3MTMPozchVfbgEm')
       .then(res => {
         console.log(res.data)
-        setImage(...res.data)
+        setImage(res.data)
       })
       .catch(err => {
         debugger
@@ -31,7 +28,7 @@ export default function App (props) {
       <p>
         NASA Photo of the Day <span role="img" aria-label='go!'>🚀</span>!
       </p>
-      <Image image = {image} setImage = {setImage}/>
+      <Image image={image} setImage={setImage}/>
     </div>
   );
 }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,24 +3,26 @@ import ImageHeader from './ImageHeader.js'
 import ImageSub from './ImageSub'
 
 const Image = props => {
-    const {date, explanation, title} = props;
+    const {image, setImage} = props;
     
     return(
-        <div className = 'image-border'>
+        <div className = 'image-border'> 
+            {image && <>
             <ImageHeader
-                title = {props.title}
+                title = {image.title}
             />
             <div className = 'image-wrapper'>
                 <img 
                     alt = "photo of the day"
                     className = 'image'
-                    src = {props.hdurl}
+                    src = {image.hdurl}
                 />
             </div>
             <ImageSub
-                date = {props.date}
-                explanation = {props.explanation}
+                date = {image.date}
+                explanation = {image.explanation}
             />
+            </>}
         </div>
     );
 };
